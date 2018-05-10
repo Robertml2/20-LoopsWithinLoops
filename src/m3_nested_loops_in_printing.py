d17e6172b4f013be2a94e30030fbd41303bf6998
robertml2 <robertm2@rose-hulman.edu>
2018-05-09 21:49:07 -0400

    commit

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -205,7 +205,7 @@ def alternating_brackets(m, n):
     Precondition:  m and n are positive integers with m >= n.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -213,6 +213,13 @@ def alternating_brackets(m, n):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
+    for k in range(m):
+        for j in range(m-k):
+            if (m-k)>=n and j%2==0:
+                print('[', end='')
+            if (m-k)>=n and j%2 ==1:
+                print(']', end='')
+        print()
 
 
 def run_test_triangle_same_number_in_each_row():
@@ -248,7 +255,7 @@ def triangle_same_number_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -256,6 +263,10 @@ def triangle_same_number_in_each_row(r):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
+    for k in range(r):
+        for j in range(k+1):
+            print(k+1, end='')
+        print()
 
 
 def run_test_triangle_all_numbers_in_each_row():
